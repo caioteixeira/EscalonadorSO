cc138c500189a2e7b42f8f1f9d109ffeaafbf2b6
Caio Vinicius Marques Teixeira <caio.vinicius.teixeira@usp.br>
2014-10-26 22:48:01 -0200

    Inicialização tabela de processos

--- Escalonador.java
+++ Escalonador.java
@@ -1,16 +1,29 @@
 import java.util.LinkedList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Escalonador{
-
-	BCP[] tabelaDeProcessos;
-	LinkedList<BCP> prontos;
-	LinkedList<BCP> bloqueados;
-
-	
+	static BCP[] tabelaDeProcessos;
+	static LinkedList<BCP> prontos;
+	static LinkedList<BCP> bloqueados;
 	public static void main(String[] args)
 	{
-		//Código...
-	} 
+		inicializa();		
+	}
+
+	static void inicializa()
+	{
+		tabelaDeProcessos = new BCP[10];
+
+		for(int i = 1; i <= 10; i++)
+		{
+			String n = i==10?"10":"0"+i;
+			tabelaDeProcessos[i] = new BCP("processos/"+n+".txt");
+		}
+	}
+	
+	 
 }
 
 enum Estado
@@ -21,9 +34,33 @@ enum Estado
 }
 
 class BCP{
+
+	BCP(String programa)
+	{
+		comandos = new LinkedList<String>();
+
+		File arquivo = new File(programa);
+
+		try
+		{
+			Scanner sc = new Scanner(arquivo);
+
+			while(sc.hasNextLine())
+			{
+				System.out.println(sc.nextLine());
+			}
+
+			sc.close();
+		}
+		catch(FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
 	int programCounter = 0;
 	Estado estado;
 	int x = 0;
 	int y = 0;
-	String[] comandos;
+	LinkedList<String> comandos;
 }
